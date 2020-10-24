bca593f3ad426ebdd602cb0c97c5bdc883ad044c
sergioadll <sergio.adll@hotmail.com>
2020-10-24 12:09:38 +0000

    excuse generator

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,25 @@
 /* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+let random = (max, min) => Math.floor(Math.random() * (max - min)) + min;
+let excusesArr = [
+  ["Ramón ", "Some random dude ", "My sister ", "Laura "],
+  ["ate ", "screwed ", "stole ", "found "],
+  ["my aunt ", "my dog ", "all my work ", "my dreams "],
+  ["in the gutter!", "at the mall! ", "in the street!", "in Madrid!"]
+];
+let excuses = () => {
+  let excuse = "";
+  for (let part of excusesArr) {
+    excuse = excuse.concat(part[random(0, part.length)]);
+    console.log(excuse);
+  }
+  return excuse;
+};
+window.onload = () => {
+  document.querySelector("#excuseBtn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuses();
+  });
+  document.querySelector("#excuse").innerHTML = excuses();
 };
